58a1941c5ffe343f4fc2090b1fa60704f75125fe
Angel <angel.dealba.garcia@gmail.com>
2021-06-27 18:56:34 -0500

    the reload page problem has been solved

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -5,8 +5,8 @@ import Colors from './Colors.json';
 const QuoteBox = () => {
 
     // varibales of state that are going to help me assing the index of my jsons. 
-    const [indexQuote, setIndexQuote] = useState(0)
-    const [indexColors, setIndexColors] = useState(0)
+    const [indexQuote, setIndexQuote] = useState(Math.floor(Math.random()*102))
+    const [indexColors, setIndexColors] = useState(Math.floor(Math.random()*20))
 
 
     return(
@@ -14,7 +14,7 @@ const QuoteBox = () => {
         <div className = 'quoteBox'>
 
                 {/*Storing my quotes and authors in a h4 and h5 heading tags*/}
-               <h4><i class="fa fa-quote-left" aria-hidden="true"></i> {Data[indexQuote].quote}</h4>
+               <h4><i className="fa fa-quote-left" aria-hidden="true"></i> {Data[indexQuote].quote}</h4>
                <h5>{Data[indexQuote].author}</h5>
                 
                 {/*buttons*/}
@@ -22,10 +22,11 @@ const QuoteBox = () => {
                 <button style={Colors[indexColors]} className='button1'><i className="fa fa-twitter"></i></button>
                 </div>
 
+                
                {/*Button with onClick function to change quote and color*/}
                <button  style={Colors[indexColors]} className ='button' type = 'button' onClick={() => {
-                   setIndexQuote(Math.floor(Math.random()*102))
-                   setIndexColors(Math.floor(Math.random()*20))
+                 setIndexQuote(Math.floor(Math.random()*102))
+                 setIndexColors(Math.floor(Math.random()*20))  
                 }
               }>
                 New Quote</button>   
@@ -33,6 +34,8 @@ const QuoteBox = () => {
         </div>
     )
 }
+
+
 
 export default QuoteBox;
 
