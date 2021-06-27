0bdaa338be504a40232883aa2fadf327b4502720
Angel <angel.dealba.garcia@gmail.com>
2021-06-26 21:47:09 -0500

    styling the main Quote Box container

--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -3,11 +3,16 @@ import Data from './Quotes.json'
 
 const QuoteBox = () => {
     const [indexQuote, setIndexQuote] = useState(Math.floor(Math.random()*((100+1)-1)))
-    console.log(Data.length)
+    
+
+
     return(
         <div className = 'quoteBox'>
                <h4>{Data[indexQuote].quote}</h4>
-               <button><i className="fa fa-twitter"></i></button>
+               <h5>{Data[indexQuote].author}</h5>
+                <div className = 'twitter'>
+               <button className='button1'><i className="fa fa-twitter"></i></button>
+               </div>
                <button className ='button' type = 'button' onClick={() => setIndexQuote(indexQuote + 1)}>
                   New Quote</button>   
         </div>
