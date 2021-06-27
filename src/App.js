b115dab6c7445dbe9bc0699859e309f1988e6318
Angel <angel.dealba.garcia@gmail.com>
2021-06-26 21:10:54 -0500

    Adding more functions in our quotes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import Data from './Quotes.json'
 import Container from './Container';
 import './main.css'
 
 function App() {
-  console.log(Data)
   return (
     <div className="App">
       <Container />
--- src/QuoteBox.js
+++ src/QuoteBox.js
@@ -1,7 +1,15 @@
+import { useState } from 'react'
+import Data from './Quotes.json'
+
 const QuoteBox = () => {
+    const [indexQuote, setIndexQuote] = useState(Math.floor(Math.random()*((100+1)-1)))
+    console.log(Data.length)
     return(
-        <div className="quoteBox">
-            This is my div
+        <div className = 'quoteBox'>
+               <h4>{Data[indexQuote].quote}</h4>
+               <button><i className="fa fa-twitter"></i></button>
+               <button className ='button' type = 'button' onClick={() => setIndexQuote(indexQuote + 1)}>
+                  New Quote</button>   
         </div>
     )
 }
